2ffd23fec0b3115f238b82a09cb6c2be88a06e32
grenade361 <mvuong361@gmail.com>
2025-02-14 10:59:01 +0700

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float laneSpeed;
+    public float jumpLength;
+    public float jumpHeight;
+    public float slideLength;
+    private bool jumping=false;
+    private float jumpStart;
+    private bool sliding = false;
+    private float slideStart;
+    
+
+
+    private Animator anim;
+    private Rigidbody rb;
+    private BoxCollider boxCollider;
+    private int currentLane = 1;
+    private Vector3 verticalTargetPosition;
+    private Vector3 boxColliderSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+        boxColliderSize = boxCollider.size;
+    }
+        // Update is called once per frame
+        void Update ()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                ChangeLane(-1);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ChangeLane(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Slide();
+            }
+
+            if (jumping)
+            {
+                float ratio = (transform.position.z - jumpStart) / jumpLength;
+                if (ratio >= 1f)
+                {
+                    jumping = false;
+                    anim.SetBool("Jumping", false);
+                }
+                else
+                {
+                    verticalTargetPosition.y = Mathf.Sin(ratio * Mathf.PI) * jumpHeight;
+
+                }
+            }
+            else
+            {
+                verticalTargetPosition.y = Mathf.MoveTowards(verticalTargetPosition.y, 0, 5 * Time.deltaTime);
+            }
+
+            if (sliding)
+            {
+                float ratio = (transform.position.z - slideStart) / slideLength;
+                if (ratio >= 1f)
+                {
+                    sliding = false;
+                    anim.SetBool("Sliding", false);
+                    boxCollider.size = boxColliderSize;
+                }
+            }
+
+
+            Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, laneSpeed * Time.deltaTime);
+        }
+    
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+    void ChangeLane(int direction)
+    {
+        int targetLane = currentLane + direction;
+        if (targetLane < 0 || targetLane >2)
+        return;
+        currentLane = targetLane;
+        verticalTargetPosition = new Vector3((currentLane - 1) * 2 , 0 , 0);
+    }
+    void Jump()
+    {
+        if (!jumping)
+        {
+            jumpStart = transform.position.z;
+            anim.SetFloat("JumpSpeed", speed / jumpLength);
+            anim.SetBool("Jumping", true);
+            jumping = true;
+        }
+    }
+
+    void Slide()
+    {
+        if (!jumping && !sliding)
+        {
+            slideStart = transform.position.z;
+            anim.SetFloat("JumpSpeed", speed / slideLength);
+            anim.SetBool("Sliding", true) ;
+            Vector3 newSize = boxCollider.size;
+            newSize.y = newSize.y / 2;
+            boxCollider.size = newSize;
+            sliding = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+
+    public GameObject[] obstacles;
+    public List<GameObject> newObstacles;
+    public Vector2 numberOfObstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int newNumberOfObstacles =(int) Random.Range(numberOfObstacles.x, numberOfObstacles.y);
+
+        for (int i = 0; i < newNumberOfObstacles; i++)
+        {
+            newObstacles.Add(Instantiate(obstacles[Random.Range(0, obstacles.Length)], transform ));
+            newObstacles[i].SetActive(false);
+        }
+        PositionateObstacles();
+    }
+
+    void PositionateObstacles()
+    {
+        for (int i = 0;i < newObstacles.Count;i++)
+        {
+            float posZmin = (45f / newObstacles.Count) + (45f / newObstacles.Count) * i;
+            float posZmax = (45f / newObstacles.Count) + (45f / newObstacles.Count) * i + 1;
+            newObstacles[i].transform.localPosition = new Vector3(0, 0, Random.Range(posZmin, posZmax));
+            newObstacles[i].SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            {
+                transform.position = new Vector3(0, 0, transform.position.z + 32 * 2);
+            Invoke("PositionateObstacles", 4f);
+
+            }
+    }
+}
